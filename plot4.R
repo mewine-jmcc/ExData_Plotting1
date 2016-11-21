e681e062a85ea4f0015aaa807b809897365053ca
mewine-jmcc <jane.chen@wsu.edu>
2016-11-21 14:22:52 -0800

    Add files via upload

--- plot4.R
+++ plot4.R
@@ -31,7 +31,7 @@ with(elec, {
         lines(Sub_metering_3, col = "blue")
         legend("topright", lty = c(1, 1, 1), col = c("black", "red", "blue"), 
                legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
-               bty = "n", cex = 0.5)
+               bty = "n", cex = 0.7)
         plot(Date_Time, Global_reactive_power, 
              xlab = "datetime", type = "l")
         })
